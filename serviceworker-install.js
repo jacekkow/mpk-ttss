434151b5b3368920dc7add2a28d8da172cb70060
Jacek Kowalski <Jacek@jacekk.info>
2017-05-14 22:38:36 +0200

    Do not call skipWaiting on inactive service worker

--- serviceworker-install.js
+++ serviceworker-install.js
@@ -2,7 +2,9 @@ if('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
 		navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
 			console.log('Service Worker registration successful. ', registration);
-			navigator.serviceWorker.controller.postMessage({action: 'skipWaiting'});
+			if(navigator.serviceWorker.controller) {
+				navigator.serviceWorker.controller.postMessage({action: 'skipWaiting'});
+			}
 		}).catch(function(err) {
 			console.log('Service Worker registration failed. ', err);
 		});
