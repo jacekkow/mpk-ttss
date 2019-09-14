f4c1c2cd07c911a31e543a09e328cc64dd9bf1bb
Jacek Kowalski <Jacek@jacekk.info>
2019-09-14 23:36:09 +0200

    Replace positionType=RAW with CORRECTED for proxy_tram.php

--- proxy_tram.php
+++ proxy_tram.php
@@ -1,3 +1,6 @@
 <?php
 $base_proxy = 'http://www.ttss.krakow.pl/internetservice';
+if($_GET['positionType'] ?? NULL === 'RAW') {
+	$_GET['positionType'] = 'CORRECTED';
+}
 require('proxy_common.php');
