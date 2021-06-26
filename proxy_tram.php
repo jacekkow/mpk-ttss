3b0880d96b4eccb5d158783f4bd1bc9b42965324
Jacek Kowalski <Jacek@jacekk.info>
2021-06-27 00:20:28 +0200

    Remove positionType=CORRECTED override for proxy_tram.php

--- proxy_tram.php
+++ proxy_tram.php
@@ -1,6 +1,3 @@
 <?php
 $base_proxy = 'http://www.ttss.krakow.pl/internetservice';
-if($_GET['positionType'] ?? NULL === 'RAW') {
-	$_GET['positionType'] = 'CORRECTED';
-}
 require('proxy_common.php');
